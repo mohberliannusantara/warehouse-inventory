a63ee95967e9778566247ae52be8cc5c70b1d631
mohberliannusantara <liannusantara1158@gmail.com>
2018-08-15 16:02:15 +0700

    templating

---
+++ application/controllers/Beranda.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Beranda extends MY_Controller {
+
+  public function index() {
+    $this->load->view('beranda/index');
+  }
+}
