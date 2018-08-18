6d9594aba2dfd901795f92fddb4887abff502171
bimosyah <bimosyahputro88@gmail.com>
2018-08-18 19:08:43 +0700

    fix join barang

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -59,7 +59,7 @@
                   </th>
                 </thead>
                 <tbody>
-                  <?php foreach ($barang as $key => $value): ?>
+                  <?php foreach ($barang as $value): ?>
                     <tr>
                       <td>
                         <?php echo $value->id_barang; ?>
@@ -68,7 +68,7 @@
                         <?php echo $value->nama_barang; ?>
                       </td>
                       <td>
-                        <?php echo $value->nama_kondisi; ?>
+                        <?php echo $value->kondisi; ?>
                       </td>
                       <td class="text-primary">
                         Rp. <?php echo number_format($value->harga); ?>
